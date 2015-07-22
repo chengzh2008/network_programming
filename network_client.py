444e6a668bfdfb7770d1321da18a30ab6fe3ebe9
chengzh2008 <chengzh2008@gmail.com>
2015-07-21 22:17:22 -0700

    netcat utility

--- network_client.py
+++ network_client.py
@@ -2,6 +2,25 @@
 __author__ = 'xiaoyazi'
 """Happy coding"""
 
+"""before run this program, make sure to run nc in the terminal
+nc -l 5555  // listening the tcp connection
+
+
+// netcat is a network utility (swiss army)
+// check for an open port
+nc -vnz -w 1 192.168.1.9 1-30
+// scan a range of port
+nc -vnz -w 1 192.168.1.9 1-30
+// port scanning UDP ports
+nc -vnzu -w 1 192.168.1.9 50-80
+// having a chat with netcat
+ip a s
+nc -l 2468 // one terminal
+nc 127.0.0.1 2468 // the other terminal
+
+"""
+
+
 import socket
 
 host = 'localhost'
